0faba8c7f0ced1036a87bca4f21ac157dc12d813
Liora Boccara <lioraboccara@gmail.com>
2023-08-04 11:07:45 +0300

    initiate navigation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-
 
 function App() {
   return (
