4c9e90beaaaf6d9210988996980ba9c4a700ba7f
Liora Boccara <lioraboccara@gmail.com>
2023-08-04 11:47:02 +0300

    add LogIn component to nav

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import UserList from './components/UserList';
 import AboutTeam from './components/AboutTeam';
+import Login from './components/LogIn';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/"/>
+          <Route path="/" element={<Login/>}/>
           <Route path="/find-love" element={<UserList users={users.results}/>}/>
           <Route path="/about-team" element={<AboutTeam/>}/>
           <Route path="/success-stories"/>
